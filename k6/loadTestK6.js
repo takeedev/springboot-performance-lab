7e9792545a13ab9b5b23474f474fce1b147e332d
Takee Dev <peerapolkoeram@gmail.com>
2026-03-30 17:56:09 +0700

    add vus

--- k6/loadTestK6.js
+++ k6/loadTestK6.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '1m',
+  stages: [
+    { duration: '1m', target: 100 },
+    { duration: '10m', target: 100 },
+    { duration: '2m', target: 0 },
+  ],
 };
 
 export default function () {
