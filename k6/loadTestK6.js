8c4a9e9ee4dc894636dd46e3320a7b2a3ca30bd8
Takee Dev <peerapolkoeram@gmail.com>
2026-04-02 19:19:22 +0700

    update use case failed and set heap

--- k6/loadTestK6.js
+++ k6/loadTestK6.js
@@ -3,13 +3,17 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 100 },
-    { duration: '10m', target: 100 },
-    { duration: '2m', target: 0 },
+    { duration: '5m', target: 100 },
+    { duration: '3m', target: 30 },
+    { duration: '2m', target: 50 },
   ],
 };
 
 export default function () {
-  http.get('http://host.docker.internal:8080/api/slow');
+  if (Math.random() < 0.2) {
+    http.get('http://host.docker.internal:8080/api/error');
+  } else {
+    http.get('http://host.docker.internal:8080/api/slow');
+  }
   sleep(1);
 }
--- src/main/java/takee/dev/performance/controller/Controller.java
+++ src/main/java/takee/dev/performance/controller/Controller.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/slow")
     public String slow() throws InterruptedException {
+        var strList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            strList.add("test");
+        }
         Thread.sleep(200);
         return "ok";
     }
