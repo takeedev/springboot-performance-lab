5e575214ba724bb98adde7756ec77e5af766f72d
Takee Dev <peerapolkoeram@gmail.com>
2026-04-30 21:59:23 +0700

    update controller

--- src/main/java/takee/dev/performance/controller/Controller.java
+++ src/main/java/takee/dev/performance/controller/Controller.java
@@ -1,25 +1,25 @@
 package takee.dev.performance.controller;
 
+import static java.lang.Math.sqrt;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static java.lang.Math.sqrt;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-    @GetMapping("/slow")
-    public String slow() {
-        for (int i = 0; i < 1000; i++) {
-            Thread.startVirtualThread(() -> {
-                while (true) {
-                    sqrt(Math.random());
-                }
-            });
-        }
-        return "ok";
+  @GetMapping("/slow")
+  public String slow() {
+    for (int i = 0; i < 1000; i++) {
+    Thread.startVirtualThread(
+          () -> {
+            while (true) {
+              sqrt(Math.random());
+            }
+          });
     }
-
+    return "ok";
+  }
 }
